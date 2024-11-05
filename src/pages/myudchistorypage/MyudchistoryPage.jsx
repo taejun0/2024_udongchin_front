2be4b8ac239b5dd92ed1db6c638bc0d7d06ad9e0
taejun0 <xownswns@naver.com>
2024-11-05 10:06:41 +0900

    feature: 내 우동친 기록 페이지 구현

--- src/pages/myudchistorypage/MyudchistoryPage.jsx
+++ src/pages/myudchistorypage/MyudchistoryPage.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import * as S from "./styled";
+import { MyudchistoryStepOne } from "@components/specific/myudchistorySteps/MyudchistoryStepOne";
+import { MyudchistoryStepTwo } from "@components/specific/myudchistorySteps/MyudchistoryStepTwo";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export const MyudchistoryPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const currentStep = new URLSearchParams(location.search).get("step") || "1";
+
+  const handleNextStep = () => {
+    navigate(`?step=${parseInt(currentStep) + 1}`);
+  };
+
+  const handlePrevStep = () => {
+    if (parseInt(currentStep) > 1) {
+      navigate(`?step=${parseInt(currentStep) - 1}`);
+    }
+  };
+
   return (
-    <>
-    
-    </>
-  )
-}
+    <S.Wrapper>
+      {currentStep === "1" && <MyudchistoryStepOne onNext={handleNextStep} />}
+      {currentStep === "2" && <MyudchistoryStepTwo />}
+    </S.Wrapper>
+  );
+};
